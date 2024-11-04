a3bb89da76d5a68f51b4fdf51c85cae56ff84d08
b.beseda <b.beseda@webvork.com>
2024-11-04 20:19:04 +0300

    debug

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const API_BASE = 'https://prometheussiptelepony.space/proekt-db/api';
 
 // Функция для входа
+export const login = (email, password) =>
+  axios.post(`${API_BASE}/user/login`, { email, password });
+
+// Функция для входа
 export const login = async (email, password) => {
   try {
     const response = await axios.post(`${API_BASE}/user/login`, { email, password });
@@ -22,24 +26,24 @@ export const login = async (email, password) => {
 // Функция для получения списка заказов с пагинацией
 export const fetchAllOrders = (token, offset = 0, limit = 10) =>
   axios.get(`${API_BASE}/orders/`, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: { token: `${token}` },
     params: { offset, limit }
   });
 
 // Функция для создания заказа
 export const createOrder = (orderData, token) =>
   axios.post(`${API_BASE}/orders/create/`, orderData, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: { token: `${token}` },
   });
 
 // Функция для получения списка заказов
 export const fetchOrders = (token) =>
   axios.get(`${API_BASE}/orders`, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: { token: `${token}` },
   });
 
 // Функция для обновления статуса заказа
 export const updateOrderStatus = (statusData, token) =>
   axios.post(`${API_BASE}/orders/change_order_status/`, statusData, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: { token: `${token}` },
   });
